fe57219deaffc35fae37b4b524cb246e5f472717
roninyymi <102995640+roninyymi@users.noreply.github.com>
2022-04-11 22:22:13 +0300

    Create Scripts2.js

---
+++ Scripts2.js
@@ -0,0 +1,13 @@
+/// RemoveWarningToast.js
+const myInterval = setInterval(ToastClean, 1000);
+
+function ToastClean() {
+    const toasts = document.querySelectorAll('.toast-root');
+    if (toasts.length > 0) {
+        toasts.forEach(toast => toast.remove());
+        console.log("Warning toast removed! t. WTR9002");
+        clearInterval(myInterval);
+    } else {
+        console.log("tick");
+    }
+}
