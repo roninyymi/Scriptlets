e06c2682bcd385d5666b76f1d8717a9180043e63
roninyymi <102995640+roninyymi@users.noreply.github.com>
2022-04-11 16:31:39 +0300

    Delete Scriptlets.js

--- Scriptlets.js
+++
@@ -1,9 +0,0 @@
-/// RemoveWarningToast.js
-function delay(time) {
-    return new Promise(resolve => setTimeout(resolve, time));
-}
-delay(1000).then(() => {
-    const toasts = document.querySelectorAll('.toast-root');
-    toasts.forEach(toast => toast.remove());
-    console.log("Warning toast removed!");
-});
