04757153ed6804974ba4285cd81f75c0c16d7cc5
roninyymi <102995640+roninyymi@users.noreply.github.com>
2022-04-11 18:11:33 +0300

    Create warning_toast_remover_9001.user.js

---
+++ warning_toast_remover_9001.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         Warning Toast Remover 9001
+// @match        https://ylilauta.org/*
+// @grant        none
+// ==/UserScript==
+(function() {
+    "use strict"
+
+    function delay(time) {
+        return new Promise(resolve => setTimeout(resolve, time));
+    }
+    delay(1000).then(() => {
+        const toasts = document.querySelectorAll('.toast-root');
+        toasts.forEach(toast => toast.remove());
+        console.log("Warning toast removed!");
+    });
+
+})()
